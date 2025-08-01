512392e8e18c2bf7d3c506a430a52d49e6efcef9
Lawrence Graham <lawrence.graham@icloud.com>
2025-08-01 15:45:12 +0200

    Allow external connections

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,6 +4,8 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: "0.0.0.0",   // ← allow external devices
+    port: 5173,        // default Vite port
     proxy: {
       '/api': 'http://127.0.0.1:5000'
     }
